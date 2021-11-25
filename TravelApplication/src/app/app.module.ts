7105cc4bf78b0304e048d97536d23e6c08a88eff
mahimoodShaik <mahshaik@innominds.com>
2021-11-25 22:45:48 +0530

    Add Components(Home,SignUp,SignIn)

--- TravelApplication/src/app/app.module.ts
+++ TravelApplication/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeDirective } from './home.directive';
+import { HomeComponent } from './home/home.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { SignInComponent } from './sign-in/sign-in.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeDirective,
+    HomeComponent,
+    SignUpComponent,
+    SignInComponent
   ],
   imports: [
     BrowserModule,
