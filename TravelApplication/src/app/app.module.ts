ba2c39d21673bde41b13f45a4fce389332194a8d
mahimoodShaik <mahshaik@innominds.com>
2021-11-26 00:27:31 +0530

    Home Page Updated

--- TravelApplication/src/app/app.module.ts
+++ TravelApplication/src/app/app.module.ts
@@ -1,9 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeDirective } from './home.directive';
 import { HomeComponent } from './home/home.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
 import { SignInComponent } from './sign-in/sign-in.component';
@@ -11,15 +10,16 @@ import { SignInComponent } from './sign-in/sign-in.component';
 @NgModule({
   declarations: [
     AppComponent,
-    HomeDirective,
+    routingComponents,
     HomeComponent,
     SignUpComponent,
     SignInComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
+  
   providers: [],
   bootstrap: [AppComponent]
 })
