c1e8ff29134c3cb50b4e4d96f0f7435012297544
mahimoodShaik <mahshaik@innominds.com>
2021-11-26 11:52:35 +0530

    Home Page Design completed

--- TravelApplication/src/app/app-routing.module.ts
+++ TravelApplication/src/app/app-routing.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { SignInComponent } from './sign-in/sign-in.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
 
 const routes: Routes = [
- {path:'home', component: HomeComponent,},
+ {path:'', component: HomeComponent,},
+ {path:'SignIn',component:SignInComponent},
+ {path:'SignUp',component:SignUpComponent},
+
 ];
 
 
