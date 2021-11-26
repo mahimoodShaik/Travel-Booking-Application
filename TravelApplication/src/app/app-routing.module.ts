c1823b9e361a0ba4477e30e64ce81469c6e8bbd9
mahimoodShaik <mahshaik@innominds.com>
2021-11-27 00:26:34 +0530

    Components Updated

--- TravelApplication/src/app/app-routing.module.ts
+++ TravelApplication/src/app/app-routing.module.ts
@@ -5,10 +5,9 @@ import { SignInComponent } from './sign-in/sign-in.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
 
 const routes: Routes = [
- {path:'', component: HomeComponent,},
+ {path:'', component: HomeComponent},
  {path:'SignIn',component:SignInComponent},
  {path:'SignUp',component:SignUpComponent},
-
 ];
 
 
