dc4043dd809b72a60a2ac5207ded6edde14b664f
mahimoodShaik <mahshaik@innominds.com>
2021-11-27 20:20:32 +0530

    SignPage Updated

--- TravelApplication/src/app/app-routing.module.ts
+++ TravelApplication/src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { SignInComponent } from './sign-in/sign-in.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
+import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
 
 const routes: Routes = [
  {path:'', component: HomeComponent},
  {path:'SignIn',component:SignInComponent},
  {path:'SignUp',component:SignUpComponent},
+ {path:'SignUp',component:SignUpComponent}
 ];
 
 
